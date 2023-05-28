73f6c45991f29816f94ff0b034c1b5d2c2a7b27c
mavargasa <monikavargas@gmail.com>
2023-05-28 15:56:15 -0500

    terminado

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function verificarDivisibilidad() {
-  var numero = parseInt(document.getElementById('numeroInput').value);
-  var resultadoDiv = document.getElementById('resultadoDiv');
-  if (numero % 2 === 0) {
-    resultadoDiv.textContent = numero + ' es divisible entre 2.';
-  } else {
-    resultadoDiv.textContent = numero + ' no es divisible entre 2.';
-  }
+// Crea un array vacío
+var numerosAleatorios = [];
+
+// Genera 10 números aleatorios y guardarlos en el array de acuerdo a ejercicio
+for (var i = 0; i < 10; i++) {
+  var numero = Math.floor(Math.random() * 100) + 1;
+  console.log(numero);
+  numerosAleatorios.push(numero);
 }
 
+// Mostrar el resultado en la consola
+console.log("Números aleatorios:", numerosAleatorios);          
