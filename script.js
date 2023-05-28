13244e161d97c9aa38b2e892cf218d6ec39bd9d8
mavargasa <monikavargas@gmail.com>
2023-05-28 14:22:31 -0500

    terminado

---
+++ script.js
@@ -0,0 +1,10 @@
+function verificarDivisibilidad() {
+  var numero = parseInt(document.getElementById('numeroInput').value);
+  var resultadoDiv = document.getElementById('resultadoDiv');
+  if (numero % 2 === 0) {
+    resultadoDiv.textContent = numero + ' es divisible entre 2.';
+  } else {
+    resultadoDiv.textContent = numero + ' no es divisible entre 2.';
+  }
+}
+
